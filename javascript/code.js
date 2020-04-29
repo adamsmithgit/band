1d315b12b8e34bc2402df221314654fbdf2d6c39
Adam Smith <63020278+adamsmithgit@users.noreply.github.com>
2020-04-29 14:32:49 -0700

    Add validation, windows onload

--- javascript/code.js
+++ javascript/code.js
@@ -1,35 +1,52 @@
-var showDate = new Date("April 29, 2020 19:00:00");
-var dateDisplay = document.getElementById('dateOfShow');
-var dateToday = Date.now();
-dateDisplay.innerHTML = showDate.toDateString();
-if (dateToday > showDate) {
-    document.getElementById("showButton").disabled = true;
-    document.getElementById("showButton").style.background = "red";
-    document.getElementById("showButton").innerHTML = "Sorry Date Passed";
+
+function setTicketButton() {
+    var showDate = new Date("April 26, 2020 19:00:00");
+    var dateDisplay = document.getElementById('dateOfShow');
+    var dateToday = Date.now();
+    dateDisplay.innerHTML = showDate.toDateString();
+    if (dateToday > showDate) {
+        document.getElementById("showButton").disabled = true;
+        document.getElementById("showButton").style.background = "red";
+        document.getElementById("showButton").innerHTML = "Sorry Date Passed";
+    }
 }
 
-var favoriteColor = prompt("Do you prefer a black or blue background?");
-if (favoriteColor.toLowerCase().includes("black")) {
-    document.getElementById('page').style.background = "black";
-    } else {
-        if (favoriteColor.toLowerCase().includes("blue")) {
-            document.getElementById('page').style.background = "blue";
-        } 
-    }
-alert("");
-
-var response = "";
-var favoriteSong = prompt("Which song should we add to our next album? 'Give it Up' or 'Tomorrow Today'");
-if (favoriteSong.toLowerCase().includes("give")) {
-    response = "We like Give it Up too."
-    } else {
-        if (favoriteSong.toLowerCase().includes("today")) {
-        response = "Tomorrow Today is a great song."
+function setBackgroundColor() {
+    var favoriteColor = "color";
+    var currentPrompt = "Do you prefer a black or blue background?";
+    do {    
+        favoriteColor = prompt(currentPrompt, "black");
+        currentPrompt = "Sorry you can only choose 'black' or 'blue.' Please re-enter your response.";
+    } while (favoriteColor.toLowerCase() != "black" && favoriteColor.toLowerCase() != "blue");
+        if (favoriteColor.toLowerCase().includes("black")) {
+        document.getElementById('page').style.background = "black";
         } else {
-            response = "Unfortunately we can't understand your answer."
+            if (favoriteColor.toLowerCase().includes("blue")) {
+                document.getElementById('page').style.background = "blue";
+            } 
+        }
+}   
+
+function favSong() {
+    var response = "";
+    var currentPrompt = "Which song should we add to our next album? 'Give it Up' or 'Tomorrow Today'";
+    var favoriteSong = "default";
+    do {
+        favoriteSong = prompt(currentPrompt, "Enter your vote here");
+        currentPrompt = "Sorry we did not understand your answer. Which song should we add to our next album? 'Give it Up' or 'Tomorrow Today'";
+    } while (!favoriteSong.toLowerCase().includes("giv") && !favoriteSong.toLowerCase().includes("tod"));
+    if (favoriteSong.toLowerCase().includes("giv")) {
+        document.getElementById("favSong").innerHTML = "Give it Up"
+    } else {
+    if (favoriteSong.toLowerCase().includes("tod")) {
+        document.getElementById("favSong").innerHTML = "Tommorow Today";
         }
     }
+}
 
-alert("Thanks for participating " + response);
-
+window.onload = function () {
+    setTicketButton();
+    this.favSong();
+    this.setBackgroundColor();
+}
 
