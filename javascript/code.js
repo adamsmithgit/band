fd9cc93a80ebba2db0ed202ec4e70f01802b99e6
Adam Smith <63020278+adamsmithgit@users.noreply.github.com>
2020-04-29 05:08:16 -0700

    Add interactive prompts

--- javascript/code.js
+++ javascript/code.js
@@ -7,3 +7,29 @@ if (dateToday > showDate) {
     document.getElementById("showButton").style.background = "red";
     document.getElementById("showButton").innerHTML = "Sorry Date Passed";
 }
+
+var favoriteColor = prompt("Do you prefer a black or blue background?");
+if (favoriteColor.toLowerCase().includes("black")) {
+    document.getElementById('page').style.background = "black";
+    } else {
+        if (favoriteColor.toLowerCase().includes("blue")) {
+            document.getElementById('page').style.background = "blue";
+        } 
+    }
+alert("");
+
+var response = "";
+var favoriteSong = prompt("Which song should we add to our next album? 'Give it Up' or 'Tomorrow Today'");
+if (favoriteSong.toLowerCase().includes("give")) {
+    response = "We like Give it Up too."
+    } else {
+        if (favoriteSong.toLowerCase().includes("today")) {
+        response = "Tomorrow Today is a great song."
+        } else {
+            response = "Unfortunately we can't understand your answer."
+        }
+    }
+
+alert("Thanks for participating " + response);
+
+
